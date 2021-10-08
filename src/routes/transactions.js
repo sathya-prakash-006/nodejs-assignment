a73686c535b7abd3b7ee4a820fee06fb4f7a8162
sathya <sathya.prakash@perennailsys.com>
2021-10-08 08:46:18 +0530

    pagination for transaction

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const csvController = require("../controllers/transactions-controller");
+const csvController = require("../services/transaction");
 const upload = require("../middlewares/upload");
 const authJwt = require("../middlewares/auth-token");
 // To bulk upload csv files
